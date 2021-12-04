3713f6d1449bf4778a80154fac5d24e281328fc5
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-12-05 00:56:59 +0300

    test

---
+++ test-admin/src/App.js
@@ -0,0 +1,15 @@
+// in src/App.js
+import * as React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+// const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const dataProvider = jsonServerProvider(`http://localhost:8080/api/dashboard`);
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
+
+export default App;
